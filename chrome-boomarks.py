19e3e4b3cf365683f78aa3cf8fb6add4b2db0b41
patrickrayreed <55840486+patrickrayreed@users.noreply.github.com>
2021-12-06 06:58:34 -0500

    Update chrome-boomarks.py

--- chrome-boomarks.py
+++ chrome-boomarks.py
@@ -1,11 +1,7 @@
 import chrome_bookmarks
-import csv
-f = open("bookmarks.csv", "a", newline="")
-writer = csv.writer(f)
+
 for folder in chrome_bookmarks.folders:
     print(folder.name)
     print(folder.folders)
 for url in chrome_bookmarks.urls:
-    writer.writerow(url.url)
-    writer.writerow(url.name)
-
+    print(url.url, url.name)
